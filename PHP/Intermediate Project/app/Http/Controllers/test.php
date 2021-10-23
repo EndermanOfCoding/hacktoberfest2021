a8d6478d616750a2138062437f2b99c8c448bd51
IAmTobeyMarshall <tobeymarshall454@gmail.com>
2021-10-23 12:14:00 +0600

    PHP Intermediate Project

---
+++ PHP/Intermediate Project/app/Http/Controllers/test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+
+class test extends Controller
+{
+    public function index(){
+    	echo "test";
+    }
+
+    
+}
---
+++ PHP/Intermediate Project/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function(){
+	echo "index page";
+});
+
+
+Route::get('/login', 'App\Http\Controllers\loginController@index');
+Route::post('/login', 'App\Http\Controllers\loginController@verify');
+
+Route::get('/home', 'App\Http\Controllers\homeController@index');
+Route::get('/logout', 'App\Http\Controllers\logoutController@index');
+
+Route::get('/create', 'App\Http\Controllers\homeController@create');
+Route::post('/create', 'App\Http\Controllers\homeController@insert');
+
+Route::get('/stdlist', 'App\Http\Controllers\homeController@stdlist');
+Route::get('/details/{id}', 'App\Http\Controllers\homeController@details');
+
+Route::get('/edit/{id}', 'App\Http\Controllers\homeController@edit');
+Route::post('/edit/{id}', 'App\Http\Controllers\homeController@update');
+
+Route::get('/delete/{id}', 'App\Http\Controllers\homeController@delete');
+Route::post('/delete/{id}', 'App\Http\Controllers\homeController@destroy');
